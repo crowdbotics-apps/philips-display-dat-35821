8bbfec69bae2beb7e70523d79bcb98be1fb78d3f
Crowdbotics <team@crowdbotics.com>
2022-06-20 19:02:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "philips_display_dat_35821",
+  url: "https://philips_display_dat_35821.botics.co",
+  api: "https://philips_display_dat_35821.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
